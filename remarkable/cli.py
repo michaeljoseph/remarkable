94a88a1f449dd1b5864bdbf42a2290a9d031f2b6
Michael Joseph <michaeljoseph+github@gmail.com>
2013-10-13 14:46:33 +0200

    Refactor and test remark rendering

--- remarkable/cli.py
+++ remarkable/cli.py
@@ -14,7 +14,6 @@ Options:
 import logging
 
 from docopt import docopt
-
 from jinja2 import Environment, PackageLoader
 
 import remarkable
@@ -22,27 +21,31 @@ import remarkable
 log = logging.getLogger(__name__)
 
 
-def main():
-    arguments = docopt(__doc__, version=remarkable.__version__)
-    debug = arguments['--debug']
-    logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
-    log.debug('arguments: %s', arguments)
-
-    if arguments['remark']:
-        file_name = arguments['<path-to-markdown-file>']
-        html_file_name = '%s.html' % file_name
-
-        html = render_remark(open(file_name).read())
-        with open(html_file_name, 'w') as html_file:
-            html_file.write(html)
-        log.info('Created %s' % html_file_name)
-
-
-def render_remark(markdown):
-    return render_template('remark.html', markdown=markdown)
-
-
 def render_template(template_name, context):
     return Environment(
         loader=PackageLoader('remarkable', 'templates')
     ).get_template(template_name).render(context)
+
+
+def remark(arguments):
+    file_name = arguments['<path-to-markdown-file>']
+    html_file_name = '%s.html' % file_name
+
+    html = render_template(
+        'remark.html',
+        dict(markdown=open(file_name).read())
+    )
+    with open(html_file_name, 'w') as html_file:
+        html_file.write(html)
+    log.info('Created %s' % html_file_name)
+
+
+def main():
+    arguments = docopt(__doc__, version=remarkable.__version__)
+
+    logging.basicConfig(
+        level=logging.DEBUG if arguments['--debug'] else logging.INFO
+    )
+    log.debug('arguments: %s', arguments)
+    if arguments['remark']:
+        remark(arguments)
