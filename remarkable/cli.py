c3f176c2d4f4c177679c2c7b5c308e245a91311a
Michael Joseph <michaeljoseph+github@gmail.com>
2013-10-13 12:01:41 +0200

    Implement remark presentation

--- remarkable/cli.py
+++ remarkable/cli.py
@@ -2,8 +2,8 @@
 remarkable.
 
 Usage:
-  remarkable [options] command <param> <another_params>
   remarkable [options] another-command <param>
+  remarkable [options] remark <path-to-markdown-file>
 
   remarkable -h | --help
 
@@ -14,9 +14,11 @@ Options:
 
   -h --help             Show this screen.
 """
+import logging
 
 from docopt import docopt
-import logging
+
+from jinja2 import Environment, PackageLoader
 
 import remarkable
 
@@ -28,3 +30,19 @@ def main():
     debug = arguments['--debug']
     logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
     log.debug('arguments: %s', arguments)
+
+    if arguments['remark']:
+        file_name = arguments['<path-to-markdown-file>']
+        html_file_name = '%s.html' % file_name
+
+        html = render_remark(open(file_name).read())
+        with open(html_file_name, 'w') as html_file:
+            html_file.write(html)
+        log.info('Created %s' % html_file_name)
+
+
+def render_remark(markdown):
+    loader = PackageLoader('remarkable', 'templates')
+    env = Environment(loader=loader)
+    template = env.get_template('remark.html')
+    return template.render({'markdown': markdown})
