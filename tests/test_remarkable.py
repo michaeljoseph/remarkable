78201b8c38b9b2870dcef9bf9ce2e7c52dde6240
Michael Joseph <michaeljoseph+github@gmail.com>
2013-10-14 08:03:51 +0200

    Refactor markdown constant to base class

--- tests/test_remarkable.py
+++ tests/test_remarkable.py
@@ -5,41 +5,6 @@ from remarkable import cli
 from . import BaseTestCase
 
 
-MARKDOWN = """
-name: inverse
-layout: true
-class: center, middle, inverse
----
-# Application To Platform
-.footnote[
-    [How we used Python to scale Yola]
-    (https://github.com/michaeljoseph/application-to-platform)
-]
----
-layout: false
-background-image: url(https://raw.github.com/michaeljoseph/images/yola.png)
-.large[
-![avatar](https://raw.github.com/michaeljoseph/images/avatar.png)
-michaeljoseph   @github     @twitter
-]
-???
-.small[
-# excuses
-
-- false pretences:  backup speaker
-
-- 4 day talk
-
-- crappy slides: squirrel (show remark / reveal)
-
-- we're hiring, let me convince you / swimming the recruitment and
-hiring manager infested nerdpool during the break
-we have business cards
-]
----
-"""
-
-
 class TestRemarkTestCase(BaseTestCase):
 
     def test_remark(self):
