8fcd272df7bf47cdb180e313594fa3988c1bd275
Michael Joseph <michaeljoseph+github@gmail.com>
2013-10-14 10:12:54 +0200

    Add output directory param

--- tests/test_remarkable.py
+++ tests/test_remarkable.py
@@ -8,7 +8,7 @@ from . import BaseTestCase
 class TestRemarkTestCase(BaseTestCase):
 
     def test_remark(self):
-        sys.argv = 'remarkable remark atp.md'.split(' ')
+        sys.argv = 'remarkable remark atp.md foo'.split(' ')
         cli.main()
         self.assertTrue(os.path.exists('%s.html' % self.example_file))
 
@@ -16,6 +16,6 @@ class TestRemarkTestCase(BaseTestCase):
 class TestRevealTestCase(BaseTestCase):
 
     def test_reveal(self):
-        sys.argv = 'remarkable reveal atp.md'.split(' ')
+        sys.argv = 'remarkable reveal atp.md foo'.split(' ')
         cli.main()
         self.assertTrue(os.path.exists('%s.html' % self.example_file))
