98f8c8f393dc4551e4abc4e80bfb43ca909e73fd
Michael Joseph <michaeljoseph+github@gmail.com>
2018-10-03 16:50:50 +0200

    Use io.open and test with 3.6

--- remarkable/util.py
+++ remarkable/util.py
@@ -1,13 +1,14 @@
+from io import open
 import logging
 
 log = logging.getLogger(__name__)
 
 
 def read_file(file_name, encoding='utf-8'):
-    return open(file_name).read().decode(encoding)
+    return open(file_name, encoding=encoding).read()
 
 
 def write_file(file_name, content, encoding='utf-8'):
-    with open(file_name, 'w') as output_file:
-        output_file.write(content.encode(encoding))
+    with open(file_name, 'w', encoding=encoding) as output_file:
+        output_file.write(content)
     log.info('Created %s' % file_name)
